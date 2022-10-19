de1506e88be475ce14e9fa7ed3246b32cc73a229
Abayomi Adeoye <114443292+Abayomi5689@users.noreply.github.com>
2022-10-19 19:57:42 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints 0-putchar.c
+ *
+ * Description: prints 0-putchar.c
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "0-putchar.c";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
