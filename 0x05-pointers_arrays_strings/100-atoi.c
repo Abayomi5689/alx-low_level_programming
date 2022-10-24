69c49577139fb45f6d99a9a3f6bf3146ffa3cc63
Abayomi Adeoye <114443292+Abayomi5689@users.noreply.github.com>
2022-10-24 20:56:36 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,14 +7,14 @@
  */
 int _atoi(char *s)
 {
- short boolean;
- int i, minus, result;
+short boolean;
+int i, minus, result;
 
- i = minus = result = boolean = 0;
- minus = -1;
+i = minus = result = boolean = 0;
+minus = -1;
 
- while (s[i] != '\0')
- {
+ while (s[i]!= '\0')
+{
 if (s[i] == '-')
 minus *= -1;
 if (s[i] >= '0' && s[i] <= '9')
@@ -26,7 +26,7 @@ boolean = 1;
 else if (boolean == 1)
 break;
 i++;
- }
+}
 result *= minus;
 return (result);
 }
