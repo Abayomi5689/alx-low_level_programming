74292054240d4edc6bc7a59674cb3934551834f1
Abayomi Adeoye <114443292+Abayomi5689@users.noreply.github.com>
2022-10-24 20:53:23 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,18 +15,18 @@ int _atoi(char *s)
 
  while (s[i] != '\0')
  {
-    if (s[i] == '-')
-    minus *= -1;
-    if (s[i] >= '0' && s[i] <= '9')
-  {
-    result *= 10;
-    result -= (s[i] - '0');
-    boolean = 1;
-  }
-      else if (boolean == 1)
-      break;
-      i++;
+if (s[i] == '-')
+minus *= -1;
+if (s[i] >= '0' && s[i] <= '9')
+{
+result *= 10;
+result -= (s[i] - '0');
+boolean = 1;
+}
+else if (boolean == 1)
+break;
+i++;
  }
-      result *= minus;
-      return (result);
+result *= minus;
+return (result);
 }
